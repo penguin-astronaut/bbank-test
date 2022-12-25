d274d3e710439ea88a0fb666d1a1288d078daf64
Vadim Kasatkin <penguin2804@gmail.com>
2022-12-25 10:32:08 +0400

    other: move xsl template to templates

--- src/Models/Credits/CreditPrint.php
+++ src/Models/Credits/CreditPrint.php
@@ -13,7 +13,7 @@ class CreditPrint
         $xml = new DOMDocument();
         $xml->loadXML($this->arrayToXml($data));
         $xsl = new DOMDocument;
-        $xsl->load(__DIR__  . '/../../../utils/payment_table.xsl');
+        $xsl->load(__DIR__ . '/../../../templates/xsl/payment_table.xsl');
         $proc = new XSLTProcessor;
         $proc->importStyleSheet($xsl);
 
